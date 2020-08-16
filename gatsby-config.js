3565051fc5b9e625455d97e332483c52af18c21c
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2020-08-16 16:59:25 +0300

    master
    fixed alias pages
    added 404 error page

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: "gatsby-alias-imports",
       options: {
         aliases: {
-          layouts: "layouts/"
+          layouts: "src/layouts"
         }
       }
     },
---
+++ src/pages/404.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const NotFoundPage = () => (
+  <section>
+    <h2>Error has happened!</h2>
+    <pre>Sorry, this page is not found.</pre>
+  </section>
+)
+
+export default NotFoundPage
